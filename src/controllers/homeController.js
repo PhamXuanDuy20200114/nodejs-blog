1ac100f0af98a0dab61d37a66f1960e38c7bfb40
= <=>
2023-10-25 00:59:44 +0700

    insert into

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -11,8 +11,22 @@ const getHoiDanIT = (req, res) => {
     res.render('sample.ejs')
 }
 const postCreateUser = (req, res) => {
-    console.log(">>> res body: ", req.body)
-    res.send('create a new user')
+    let email = req.body.email;
+    let name = req.body.name;
+    let city = req.body.city;
+    // console.log(">>> email: ", email, ">>> name: ", name, ">>>city:", city)
+
+    connection.query(
+        `INSERT INTO 
+        Users (email, name, city) 
+        VALUES (?, ?, ?)`,
+        [email, name, city],
+        function (err, results) {
+            console.log(results)
+
+            res.send('Create user success')
+        }
+    );
 }
 module.exports = {
     getHomepage, getABC, getHoiDanIT, postCreateUser
