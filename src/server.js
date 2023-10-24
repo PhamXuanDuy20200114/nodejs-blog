14dd4445fbe5cf7a18097ee5c53afaa7d7ead3a7
= <=>
2023-10-24 15:38:04 +0700

    export connection

--- src/server.js
+++ src/server.js
@@ -2,8 +2,7 @@ require('dotenv').config();
 const express = require('express'); //common js
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
-
-const mysql = require('mysql2');
+const connection = require('./config/database');
 
 const app = express() //app express
 const port = process.env.PORT || 8888 // port => hardcode 
@@ -15,20 +14,10 @@ configViewEngine(app);
 //Khai bao route
 app.use('/test', webRoutes)
 
-//test connection
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3307, //default: 3306
-    user: 'root',//default: empty
-    password: '123456',
-    database: 'hoidanit'
-});
-
 connection.query(
     'SELECT * FROM Users u',
     function (err, results, fields) {
         console.log(">>result: ", results); // results contains rows returned by server
-        console.log(">>fields: ", fields); // fields contains extra meta data about results, if available
     }
 );
 
