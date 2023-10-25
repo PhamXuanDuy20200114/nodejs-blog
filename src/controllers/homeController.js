b4e22475114e3aa69144786843096c30996144b5
= <=>
2023-10-26 01:57:12 +0700

    Query list user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,9 @@
 const connection = require('../config/database')
+const { getAllUsers } = require('../services/CRUDService')
 
-const getHomepage = (req, res) => {
-    return res.render('homepage.ejs')
+const getHomepage = async (req, res) => {
+    let results = await getAllUsers();
+    return res.render('homepage.ejs', { listUsers: results })
 }
 const getABC = (req, res) => {
     res.send('Check ABC')
